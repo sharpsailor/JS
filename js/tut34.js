cc9ad5b10213bb5c28209d8863db4cce75aaa01e
sharpsailor <sharpsailor13@gmail.com>
2022-10-05 14:46:35 +0530

    Ajs

---
+++ js/tut34.js
@@ -0,0 +1,9 @@
+console.log("Tut 34");
+setTimeout(() => {
+  for (let index = 1; index <= 45; index++) {
+    const element = index;
+
+    console.log("This is index number " + index);
+  }
+},100);
+console.log("done printing");
